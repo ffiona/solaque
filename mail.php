8d2dadcc56fe9f9b33f98fb2027481c09f2ebb62
ffiona <fionaaya@gmail.com>
2017-09-25 17:34:50 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,11 +3,11 @@
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $email = filter_var(trim($_POST["Email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($email)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Oops! There was a problem with your submission. Please complete the form and try again.";
